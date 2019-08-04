a0ec7d4d15430a5e493b002fa1c770ce249e4d9f
Odin <sergius.la@gmail.com>
2019-08-04 01:40:26 -0700

    setup_scriot

---
+++ setup_gitNotes.sh
@@ -0,0 +1,12 @@
+mkdir -pv ~/Desktop/GitNotes
+cd ~/Desktop/GitNotes/
+git clone https://github.com/sergius-la/GitNotes.git
+git clone https://github.com/sergius-la/Java.git
+git clone https://github.com/sergius-la/Python.git
+git clone https://github.com/sergius-la/Swift.git
+git clone https://github.com/sergius-la/Automation.git
+git clone https://github.com/sergius-la/Algorithms.git
+git clone https://github.com/sergius-la/CodingBat-Java.git
+git clone https://github.com/sergius-la/Algorithms-Python.git
+git clone https://github.com/sergius-la/Algorithms-Java.git
+git clone https://github.com/sergius-la/Cheatsheet.git
